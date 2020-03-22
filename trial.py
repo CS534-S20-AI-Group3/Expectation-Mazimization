dc9039b08a2af809084848221b514452e026c982
Ankit <achakraborty@wpi.edu>
2020-03-22 11:40:03 -0400

    working em

--- trial.py
+++ trial.py
@@ -21,15 +21,26 @@ def cal_prob(point,clusters):
         cluster_prob = cluster_prob_a*cluster_prob_b
         #print("cluster prob",cluster_prob)
         p_point_cluster.append(cluster_prob)
+    b_k_sum = 0
+    b_k =[]
+    for i in range(0,len(clusters)):
+        b_k_sum = b_k_sum + p_point_cluster[i]*clusters[i][2]
+    for i in range(0,len(clusters)):
+        b_k.append((p_point_cluster[i]*clusters[i][2])/b_k_sum)
+    # print("likelyhood",b_k)
+    # print("sum of likelyhood",sum(b_k))
+    return b_k
+
     #print("prob without nomalization",p_point_cluster)
-    sum_cluster = sum(p_point_cluster)
-    p_cluster =[]
-    for x in p_point_cluster:
-        ans = x/sum_cluster
-        p_cluster.append(ans)
-    # print("probablities",p_cluster)
-    # print("sum of prob",sum(p_cluster))
-    return p_cluster
+    # sum_cluster = sum(p_point_cluster)
+    # p_cluster =[]
+    # for x in p_point_cluster:
+    #     ans = x/sum_cluster
+    #     p_cluster.append(ans)
+    # # print("probablities",p_cluster)
+    # # print("sum of prob",sum(p_cluster))
+    # return p_cluster
+
 def update_cluster(clusters,prob,points):
     copy_points = copy.deepcopy(points)
     for i in range(0,len(clusters)):
@@ -60,6 +71,7 @@ def update_cluster(clusters,prob,points):
         clusters[i][0][0] = mean_i_x
         clusters[i][0][1] = mean_i_y
         clusters[i][1] = var
+        clusters[i][2] = sum_prob_i/len(copy_points)
         #print("cluster",i,"updated vals",mean_i_x,mean_i_y,var)
     #print("updated cluster",clusters)
     return clusters
@@ -71,9 +83,9 @@ def update_cluster(clusters,prob,points):
 
 
 
-given_points = read_board(r"C:\Users\Ankit\Desktop\sample_EM_data1.csv")
+given_points = read_board(r"C:\Users\Ankit\Desktop\sample_EM_data.csv")
 for i in given_points:
-    plt.plot(i[0],i[1],'r.')
+    plt.scatter(i[0],i[1],c ='r',s =0.5,marker='o')
 number_of_clusters = 3
 no_points = len(given_points)
 print(no_points)
@@ -81,18 +93,18 @@ em_clusters =[]
 for i in range(0,number_of_clusters):
     random_mean = given_points[random.randint(0,no_points)]
     em_clusters.append([[random_mean[0],random_mean[1]],5,1/number_of_clusters])
-print("Initial cluster centers",em_clusters)
-prob_dist = []
-for i in range(0,10):
+for e in em_clusters:
+    plt.scatter(e[0][0],e[0][1],c = 'b',marker='d',data=[e[0][0],e[0][1]])
+for i in range(0,100):
+    prob_dist = []
     print("initial em cluster",em_clusters)
     for p in given_points:
         prob_dist.append(cal_prob(p,em_clusters))
     em_clusters=update_cluster(em_clusters,prob_dist,given_points)
     print("updated clusters",em_clusters)
-    for e in em_clusters:
-        for i in given_points:
-            plt.plot(i[0], i[1], 'r.')
-        plt.plot(e[0][0],e[0][1],'bo')
-        c = plt.Circle((e[0][0],e[0][1]),e[1],facecolor='None',fill='None',edgecolor='black')
-    plt.show()
+
+    #     c = plt.Circle((e[0][0],e[0][1]),e[1],facecolor='None',fill='None',edgecolor='black')
+for e in em_clusters:
+        plt.scatter(e[0][0],e[0][1],c = 'g')
+plt.show()
 
