1f15889fbe3041c98ce9a3986f9b738e70adab97
Ankit <achakraborty@wpi.edu>
2020-03-30 23:06:30 -0400

    added BIC, LL time based

--- em_with_plot.py
+++ em_with_plot.py
@@ -26,8 +26,8 @@ def cal_prob(point,clusters):
         # print("second mat shape", second_mat.shape)
         diff_mat_trans = np.transpose(diff_mat)
         # print("third mat shape", diff_mat_trans.shape)
-        probability_1 = 1 / (pow((2 * math.pi), dim *0.5) * pow(abs(np.linalg.det(cluster_varience)), 0.5))
-        probability_2 = np.exp(float(-0.5 * diff_mat.dot(second_mat).dot(diff_mat_trans)))
+        probability_1 = 1 / (pow((2 * math.pi), dim *0.5) * pow(abs(np.linalg.det(cluster_varience)),0.5))
+        probability_2 = math.exp(float(-0.5 * diff_mat.dot(second_mat).dot(diff_mat_trans)))
         probability = probability_1*probability_2
         # print("prob",probability)
         # print("distance",distance(point,(cluster_mean_x,cluster_mean_y)))
@@ -67,72 +67,77 @@ def cal_prob(point,clusters):
 
 # updates the mean,var and weights and sends it as a new cluster
 def update_cluster(clusters,prob_dist,points):
-    #print("M step")
+    # print("M step")
     prob = np.array(prob_dist)
     copy_points = np.array(copy.deepcopy(points))
     new_clusters = copy.deepcopy(clusters)
-    for i in range(0,len(clusters)):
-        #print("cluster", i)
+    for i in range(0, len(clusters)):
+        # print("cluster", i)
         sum_prob_i = 0
         for x in prob:
-            #print("prob of point",x)
-            sum_prob_i = x[i]+sum_prob_i
+            # print("prob of point",x)
+            sum_prob_i = x[i] + sum_prob_i
 
-        #print("sum of prob for this cluster",sum_prob_i)
-        t=0
-        mean_i_num = np.zeros(len(copy_points[0]))
+        # print("sum of prob for this cluster",sum_prob_i)
+        weight_mat = []
+        for p in prob:
+            weight_mat.append(p[i] / sum_prob_i)
+        #print("Weight mat", weight_mat)
+        t = 0
+        mean_i = np.zeros(len(copy_points[0]))
 
         for p in copy_points:
-            #print("point",p,"prob for cluster",prob[t])
-            #print("test",np.array(p)*float(prob[t][i]))
-            mean_i_num = (np.array(p)*float(prob[t][i])) + mean_i_num
-            #print("mean num",mean_i_num)
-            t=t+1
-        mean_i = np.array(mean_i_num)/sum_prob_i
-        #print("mean for clus",i,mean_i)
-        var_num = np.zeros([len(copy_points[0]),len(copy_points[0])])
-        #print("var num",var_num)
-        t=0
+            # print("point",p,"prob for cluster",prob[t])
+            # print("test",np.array(p)*float(prob[t][i]))
+            mean_i = (np.array(p) * float(weight_mat[t])) + mean_i
+            # print("mean num",mean_i_num)
+            t = t + 1
+
+        # print("mean for clus",i,mean_i)
+        var_num = np.zeros([len(copy_points[0]), len(copy_points[0])])
+        # print("var num",var_num)
+        t = 0
         for p in copy_points:
-            diff_mat = p-mean_i[np.newaxis]
+            diff_mat = p - mean_i[np.newaxis]
             diff_mat_tras = np.transpose(diff_mat)
             # print("diff mat ", diff_mat)
             # print("diff mat shape",diff_mat.shape)
             # print("diff mat trans shape", np.transpose(diff_mat[np.newaxis]).shape)
-            var_num = var_num + (float(prob[t][i]) * diff_mat_tras*diff_mat)
-            t=t+1
-        #print("var num", var_num)
-        var = var_num/sum_prob_i
-        #print("var of cluster",i, var)
-        # var = math.sqrt(var)
+            var_num = var_num + (float(weight_mat[t]) * diff_mat_tras * diff_mat)
+            t = t + 1
 
         new_clusters[i][0] = mean_i
-        new_clusters[i][1] = var
-        new_clusters[i][2] = sum_prob_i/len(copy_points)
+        new_clusters[i][1] = var_num
+        new_clusters[i][2] = sum_prob_i / len(copy_points)
 
     # print(" old cluster", i, "vals ", clusters)
     # print(" new cluster", i, "updated vals ", new_clusters)
-    #print("updated cluster",clusters)
+    # print("updated cluster",clusters)
     return new_clusters
 
 
 
-def em_clustering(data_path,no_k,means_k):
-    given_points = np.array(read_board(data_path))
+def em_clustering(given_points,no_k,bic_bool):
+
     plt.figure(1)
     ax = plt.subplot()
     # for i in given_points:
     #     ax.plot(i[0],i[1],color='red', marker='o', markersize=2)
     number_of_clusters = no_k
     no_of_dim = len(given_points[0])
-    print("no of clusters",number_of_clusters)
-    print("Number of Dimensions",no_of_dim)
     no_points = len(given_points)
-    print("NUmber of points",no_points)
     em_clusters = []#this mat will hold[mean_mat,covar_mat,weight] for each cluster
     final_prob_dist =[]#this holds the prob of ech point belonging to each cluster [[p1,p2...pk],[p1,p2,....pk]........all points ]
     best_ll = 0
+    clusters = []
+    for k in range(number_of_clusters):
+        clusters.append([])
+
     co_var_mat = np.identity(no_of_dim)#initial covar matrix
+    if(bic_bool==True):
+        ll_threshold = 2
+    else:
+        ll_threshold = 1
     #co_var_mat = [[1,0],[0,1]]
     #print("co var mat test",co_var_mat)
     weight_cluster = 1/number_of_clusters
@@ -140,11 +145,13 @@ def em_clustering(data_path,no_k,means_k):
         random_mean = given_points[random.randint(0, no_points - 1)]
         em_clusters.append([random_mean, co_var_mat, weight_cluster])
         #em_clusters.append([means_k[i],co_var_mat,weight_cluster])
-    print("Initial cluster mean , covar , weight")
-    for e in em_clusters:
-        print(e)
-    print("running EM")
-    number_iterations = 100
+    # print("Initial cluster mean , covar , weight")
+    # for e in em_clusters:
+    #     print(e)
+    # print("running EM")
+    run  = True
+    iteration = 0
+    #number_iterations = 100
     # ax.axis('equal')
     # for e in em_clusters:
     #     #circle1 = plt.Circle((e[0][0], e[0][1]),radius=e[1],color='blue',fill=False)
@@ -153,7 +160,7 @@ def em_clustering(data_path,no_k,means_k):
     #
     #     #plt.scatter(e[0][0],e[0][1],c = 'b',marker='d',data=[e[0][0],e[0][1]])
     ####### E step ##########################
-    for i in range(0, number_iterations):
+    while(run):
         prob_dist = []
         temp_ll = 0
         # print("initial em cluster",em_clusters)
@@ -164,54 +171,38 @@ def em_clustering(data_path,no_k,means_k):
         # for p in prob_dist:
         #     print(p)
         ####### M step ##########################
-        best_ll = temp_ll
-        print("Log Likelyhood iteration",i,best_ll)
+
+        #print("Log Likelyhood iteration",iteration,temp_ll)
         em_clusters = update_cluster(em_clusters, prob_dist, given_points)
         final_prob_dist = prob_dist
+        diff_t = abs(temp_ll - best_ll)
+        #print("diff", diff_t)
+        if (diff_t < ll_threshold and diff_t != float('-inf') and diff_t!=float('inf')):
+            run = False
+        best_ll = temp_ll
+        iteration = iteration+1
         # print("updated clusters mean")
         # for k in em_clusters:
         #     print(k[0])
+    point = 0
+    for p in final_prob_dist:
+        clus = p.index(max(p))
+        (clusters[clus]).append(given_points[point])
+        point = point+1
 
 
     #     #     c = plt.Circle((e[0][0],e[0][1]),e[1],facecolor='None',fill='None',edgecolor='black')
     #
-    print("final cluster",em_clusters)
+    # print("final cluster",em_clusters)
+    # print("clusters")
+    # for l in clusters:
+    #     print(l)
     # print("final prob dist")
     # for k in final_prob_dist:
     #     print(k)
-    if(no_of_dim==2):
-        color_bar = []
-        for e in em_clusters:
-            f = (random.uniform(0.1, 1), random.uniform(0, 1), random.uniform(0, 1))
-            color_bar.append(f)
-        l = 0
-        for p in final_prob_dist:
-            # print(p)
-            c = p.index(max(p))
-            # print(c)
-            ax.plot(given_points[l][0], given_points[l][1], color=color_bar[c], marker='o', markersize=10, alpha=0.4,
-                    mec=color_bar[c], mew=0)
-            l = l + 1
-            # if(c==0):
-            #     ax.plot(given_points[l][0], given_points[l][1], color='red', marker='o', markersize=2)
-            #     l=l+1
-            # elif (c == 1):
-            #     ax.plot(given_points[l][0], given_points[l][1], color='cyan', marker='o', markersize=2)
-            #     l=l+1
-            # else:
-            #     ax.plot(given_points[l][0], given_points[l][1], color='magenta', marker='o', markersize=2)
-            #     l=l+1
-        i = 0
-        for e in em_clusters:
-            #     # circle2 = plt.Circle((e[0][0], e[0][1]),radius=e[1],color='green',fill=False)
-            #     # ax.add_artist(circle2)
-            ax.plot(e[0][0], e[0][1], color=color_bar[i], marker='d', markersize=8, markeredgecolor='k',
-                    markeredgewidth=1)
-            i = i + 1
-        #     # print("final circle plotted")
-        ax.set_title('EM , LL '+str(best_ll))
 
 
+    return ([best_ll,clusters,em_clusters])
 
 
 
--- expect_max.py
+++ expect_max.py
@@ -5,8 +5,7 @@ import numpy
 import sys
 from matplotlib import pyplot as plt
 from expect_maximum_clusters import *
-from matplotlib import animation
-
+import time
 
 def k_means_clustering(data, num_clust):
     print("Running K means")
@@ -46,15 +45,95 @@ def k_means_clustering(data, num_clust):
 
     return means
     #print(len(clusters))
+def em_restart(file_path,no_k):
+    given_points = np.array(read_board(file_path))
+    start_time = time.perf_counter()
+    no_of_clusters = no_k
+    # means_k = k_means_clustering(file_path,no_of_clusters)
+    # print(" Passing means from K means to EM ")
+    # for i in range(2,12):
+    sol_ll = 0
+    sol_clusters = []
+    sol_em = []
+    restart = True
+    if(no_of_clusters==0):
+        bic_bool=True
+    else:
+        bic_bool = False
+    print("Running EM with restart")
+    print("no of clusters", no_of_clusters)
+    print("Number of Dimensions", len(given_points[0]))
+    print("NUmber of points", len(given_points))
+    if(no_of_clusters!=0):
+
+        while (restart):
+            end_time = time.perf_counter()
+            if (end_time - start_time > 9):
+                restart = False
+                print("best ll ", sol_ll)
+                print("clusters", sol_clusters)
+                print("EM mean , covar , weight", sol_em)
+                print("end time", end_time - start_time)
+                break
+            sol = em_clustering(given_points, no_of_clusters,bic_bool)
+            #print("ll",sol[0],sol[1],sol[2])
+            if(sol[0]>sol_ll or sol_ll==0):
+                sol_ll = sol[0]
+                sol_clusters = sol[1]
+                sol_em = sol[2]
+            end_time = time.perf_counter()
+
+        # if (no_of_dim == 2):
+        #     color_bar = []
+        #     for e in em_clusters:
+        #         f = (random.uniform(0.1, 1), random.uniform(0, 1), random.uniform(0, 1))
+        #         color_bar.append(f)
+        #     l = 0
+        #     for p in final_prob_dist:
+        #         # print(p)
+        #         c = p.index(max(p))
+        #         # print(c)
+        #         ax.plot(given_points[l][0], given_points[l][1], color=color_bar[c], marker='o', markersize=10,
+        #                 alpha=0.4,
+        #                 mec=color_bar[c], mew=0)
+        #         l = l + 1
+        #
+        #     i = 0
+        #     for e in em_clusters:
+        #         #     # circle2 = plt.Circle((e[0][0], e[0][1]),radius=e[1],color='green',fill=False)
+        #         #     # ax.add_artist(circle2)
+        #         ax.plot(e[0][0], e[0][1], color=color_bar[i], marker='d', markersize=8, markeredgecolor='k',
+        #                 markeredgewidth=1)
+        #         i = i + 1
+        #     #     # print("final circle plotted")
+        #     ax.set_title('EM , LL ' + str(best_ll))
+        #plt.show()
+    else:
+        print("Running BIC")
+        start_k = 1
+        run = True
+        while(run):
+            end_time = time.perf_counter()
+            if (end_time - start_time > 9):
+                run = False
+                print("best ll ", sol_ll)
+                print("clusters", sol_clusters)
+                print("EM mean , covar , weight", sol_em)
+                print("end time", end_time - start_time)
+                break
+            sol = em_clustering(given_points, start_k,bic_bool)
+            #print("ll", sol[0], sol[1], sol[2])
+            bic = start_k*np.log(len(given_points))-2*sol[0]
+            print("BIC for k ",start_k,bic)
+            if (sol[0] > sol_ll or sol_ll == 0):
+                sol_ll = sol[0]
+                sol_clusters = sol[1]
+                sol_em = sol[2]
+            start_k = start_k+1
+
 
 file_path = r"/home/ankit/git/AI/assignment_2/Expectation-Mazimization/sample_EM_data.csv"
-no_of_clusters = 4
-means_k = k_means_clustering(file_path,no_of_clusters)
-print(" Passing means from K means to EM ")
-#for i in range(2,12):
-em_clustering(file_path,no_of_clusters,means_k)
-plt.show()
-
+em_restart(file_path,0)
 # if __name__ == "__main__":
 #     if len(sys.argv) == 3:
 #         expect_max(sys.argv[1], sys.argv[2])
